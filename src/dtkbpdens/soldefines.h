fae8f8d104b37c05fc1c49e2cc120763a1aa40f2
Manuel Solano <jmsolanoalt@yahoo.com.mx>
2015-01-12 12:46:02 -0600

    dtkpoint now includes information of LED, |LED|, RoSE and RedDensGrad

--- src/dtkbpdens/soldefines.h
+++ src/dtkbpdens/soldefines.h
@@ -18,7 +18,7 @@ typedef double solreal;
 #endif
 
 #define USEPROGRESSBAR 1
-#define CURRENTVERSION "1.0.2b"
+#define CURRENTVERSION "1.0.3"
 #define PROGRAMCONTRIBUTORS "JMSA/JMHP"
 #define EPSFORELFVALUE (2.871e-05)
 #define DEFAULTPOINTSPERDIRECTION (300)
