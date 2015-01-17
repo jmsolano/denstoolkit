461f818483669b9476f6fefe98983cf1e2004034
Manuel Solano <jmsolanoalt@yahoo.com.mx>
2015-01-17 08:00:31 -0600

    Changed version of dtkfindcp

--- src/dtkfindcp/soldefines.h
+++ src/dtkfindcp/soldefines.h
@@ -18,7 +18,7 @@ typedef double solreal;
 #endif
 
 #define USEPROGRESSBAR 1
-#define CURRENTVERSION "0.1.6"
+#define CURRENTVERSION "0.1.7"
 #define PROGRAMCONTRIBUTORS "JMSA/JMHP"
 #define EPSFORELFVALUE (2.871e-05)
 #define DEFAULTPOINTSPERDIRECTION (80)
