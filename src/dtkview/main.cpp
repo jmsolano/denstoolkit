0d19cc365c305d5f084c24ac6cffa41f05b72546
Manuel Solano <jmsolanoalt@yahoo.com.mx>
2015-08-31 15:05:22 +0200

    Corrected main window icon size.

--- src/dtkview/main.cpp
+++ src/dtkview/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
    glutInit(&argc,argv);
    QApplication a(argc, argv);
    DTKMainWindow w;
-   w.setWindowIcon(QIcon(":/images/dtk64x64.png"));
+   w.setWindowIcon(QIcon(":/images/dtk32x32.png"));
    w.show();
 
    return a.exec();
