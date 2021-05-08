29a4cb9d31d6bd60afbf6b7158e620ecf0fb865d
Manuel Solano <jmsolanoalt@yahoo.com.mx>
2021-05-08 17:05:21 -0500

    Version bump to 1.5.0

--- src/dtkpropcpsoniso/dtkpropcpsoniso.cc
+++ src/dtkpropcpsoniso/dtkpropcpsoniso.cc
@@ -3,9 +3,12 @@
   
                     D  E  N  S  T  O  O  L  K  I  T
   
+                           VERSION: 1.5.0
+  
                Contributors: Juan Manuel Solano-Altamirano
                              Julio Manuel Hernandez-Perez
-          Copyright (c) 2013-2020, Juan Manuel Solano-Altamirano
+                             Luis Alfredo Nunez-Meneses
+          Copyright (c) 2013-2021, Juan Manuel Solano-Altamirano
                                    <jmsolanoalt@gmail.com>
   
    -------------------------------------------------------------------
