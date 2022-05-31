58fa0885fdb71af0e0bdd59fcfffccb1ea19f350
Manuel Solano <jmsolanoalt@yahoo.com.mx>
2022-05-31 08:03:54 -0500

    Version bump to 1.6.0

--- src/dtkplane/optflags.h
+++ src/dtkplane/optflags.h
@@ -3,12 +3,13 @@
   
                     D  E  N  S  T  O  O  L  K  I  T
   
-                           VERSION: 1.5.0
+                           VERSION: 1.6.0
   
                Contributors: Juan Manuel Solano-Altamirano
                              Julio Manuel Hernandez-Perez
                              Luis Alfredo Nunez-Meneses
-          Copyright (c) 2013-2021, Juan Manuel Solano-Altamirano
+                             Santiago Alberto Flores Roman
+          Copyright (c) 2013-2022, Juan Manuel Solano-Altamirano
                                    <jmsolanoalt@gmail.com>
   
    -------------------------------------------------------------------
