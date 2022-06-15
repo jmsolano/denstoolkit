8a2d8655aca0b683c13cf34e88f024a9a48d13c6
Manuel Solano <jmsolanoalt@yahoo.com.mx>
2022-06-15 12:26:02 -0500

    Update palette (sollibs2).

--- src/common/palette.h
+++ src/common/palette.h
@@ -56,65 +56,65 @@ public:
    void GetRGB(const size_t pos,double &r,double  &g,double  &b) const;
    void GetRGB(const size_t pos,int &r,int  &g,int  &b) const;
    inline void GetHSL(const size_t pos,double &h,double  &s,double  &l) const
-      { h=((*pal)[pos][0]); s=((*pal)[pos][1]); l=((*pal)[pos][2]); }
+      { h=(pal[pos*3+0]); s=(pal[pos*3+1]); l=(pal[pos*3+2]); }
 /* ************************************************************************** */
    Palette();
    void SelectPalette(const string &unm);
    /* cat the contents of ../expandpalette/SETPALFNCTS (see ../expandpalette/README  */
-   void SetBentcoolwarm() { pal=&bentcoolwarm; }
-   void SetBlues() { pal=&blues; }
-   void SetBugn() { pal=&bugn; }
-   void SetGnbu() { pal=&gnbu; }
-   void SetGreens() { pal=&greens; }
-   void SetGreys() { pal=&greys; }
-   void SetInferno() { pal=&inferno; }
-   void SetMagma() { pal=&magma; }
-   void SetMoreland() { pal=&moreland; }
-   void SetOranges() { pal=&oranges; }
-   void SetOrrd() { pal=&orrd; }
-   void SetPlasma() { pal=&plasma; }
-   void SetPubu() { pal=&pubu; }
-   void SetPurples() { pal=&purples; }
-   void SetRdbu() { pal=&rdbu; }
-   void SetRdylbu() { pal=&rdylbu; }
-   void SetRdylgn() { pal=&rdylgn; }
-   void SetReds() { pal=&reds; }
-   void SetSpectral() { pal=&spectral; }
-   void SetViridis() { pal=&viridis; }
-   void SetYlgn() { pal=&ylgn; }
-   void SetYlgnbu() { pal=&ylgnbu; }
-   void SetYlorbr() { pal=&ylorbr; }
-   void SetYlorrd() { pal=&ylorrd; }
+   void SetBentcoolwarm() { pal=bentcoolwarm; }
+   void SetBlues() { pal=blues; }
+   void SetBugn() { pal=bugn; }
+   void SetGnbu() { pal=gnbu; }
+   void SetGreens() { pal=greens; }
+   void SetGreys() { pal=greys; }
+   void SetInferno() { pal=inferno; }
+   void SetMagma() { pal=magma; }
+   void SetMoreland() { pal=moreland; }
+   void SetOranges() { pal=oranges; }
+   void SetOrrd() { pal=orrd; }
+   void SetPlasma() { pal=plasma; }
+   void SetPubu() { pal=pubu; }
+   void SetPurples() { pal=purples; }
+   void SetRdbu() { pal=rdbu; }
+   void SetRdylbu() { pal=rdylbu; }
+   void SetRdylgn() { pal=rdylgn; }
+   void SetReds() { pal=reds; }
+   void SetSpectral() { pal=spectral; }
+   void SetViridis() { pal=viridis; }
+   void SetYlgn() { pal=ylgn; }
+   void SetYlgnbu() { pal=ylgnbu; }
+   void SetYlorbr() { pal=ylorbr; }
+   void SetYlorrd() { pal=ylorrd; }
 /* ************************************************************************** */
 protected:
 /* ************************************************************************** */
    /* cat the contents of ../expandpalette/VECPALDEFS (see ../expandpalette/README  */
-   static vector<vector<double> > bentcoolwarm;
-   static vector<vector<double> > blues;
-   static vector<vector<double> > bugn;
-   static vector<vector<double> > gnbu;
-   static vector<vector<double> > greens;
-   static vector<vector<double> > greys;
-   static vector<vector<double> > inferno;
-   static vector<vector<double> > magma;
-   static vector<vector<double> > moreland;
-   static vector<vector<double> > oranges;
-   static vector<vector<double> > orrd;
-   static vector<vector<double> > plasma;
-   static vector<vector<double> > pubu;
-   static vector<vector<double> > purples;
-   static vector<vector<double> > rdbu;
-   static vector<vector<double> > rdylbu;
-   static vector<vector<double> > rdylgn;
-   static vector<vector<double> > reds;
-   static vector<vector<double> > spectral;
-   static vector<vector<double> > viridis;
-   static vector<vector<double> > ylgn;
-   static vector<vector<double> > ylgnbu;
-   static vector<vector<double> > ylorbr;
-   static vector<vector<double> > ylorrd;
+   const static double bentcoolwarm[768];
+   const static double blues[768];
+   const static double bugn[768];
+   const static double gnbu[768];
+   const static double greens[768];
+   const static double greys[768];
+   const static double inferno[768];
+   const static double magma[768];
+   const static double moreland[768];
+   const static double oranges[768];
+   const static double orrd[768];
+   const static double plasma[768];
+   const static double pubu[768];
+   const static double purples[768];
+   const static double rdbu[768];
+   const static double rdylbu[768];
+   const static double rdylgn[768];
+   const static double reds[768];
+   const static double spectral[768];
+   const static double viridis[768];
+   const static double ylgn[768];
+   const static double ylgnbu[768];
+   const static double ylorbr[768];
+   const static double ylorrd[768];
 /* ************************************************************************** */
-   vector<vector<double> > *pal;
+   const double *pal;
    static constexpr double oo255=1.0e0/255.0e0;
 };
 /* ************************************************************************** */
