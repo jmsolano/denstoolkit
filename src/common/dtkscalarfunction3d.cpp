1139444d43af6ad46b9cc24af5df31164170400e
Manuel Solano <jmsolano@yahoo.com.mx>
2024-02-21 18:26:13 -0600

    Add gradf and fptr to DTKScalarFunction3D

--- src/common/dtkscalarfunction3d.cpp
+++ src/common/dtkscalarfunction3d.cpp
@@ -2,11 +2,14 @@
 #include <iostream>
 using std::cout;
 #include "dtkscalarfunction3d.h"
+#include "screenutils.h"
+#include "fldtypesdef.h"
 
 bool DTKScalarFunction::prntunknfld1st=true;
 DTKScalarFunction::DTKScalarFunction() : Function3D() {
    wf=nullptr;
    sft=ScalarFieldType::NONE;
+   double (GaussWaveFunction::*selfnc)(double,double,double) = 0;
 }
 DTKScalarFunction::DTKScalarFunction(GaussWaveFunction &ugwf) : DTKScalarFunction() {
    wf=&ugwf;
@@ -14,4 +17,94 @@ DTKScalarFunction::DTKScalarFunction(GaussWaveFunction &ugwf) : DTKScalarFunctio
 }
 void DTKScalarFunction::SetScalarFunction(char t) {
    sft=Char2ScalarFieldType(t);
+   SelectScalarFunctionPtr(t);
+}
+void DTKScalarFunction::SelectScalarFunctionPtr(const char prop) {
+   return SelectScalarFunctionPtr(Char2ScalarFieldType(prop));
+}
+void DTKScalarFunction::SelectScalarFunctionPtr(const ScalarFieldType ft) {
+   switch ( ft ) {
+      case DENS :
+         selfnc=&GaussWaveFunction::EvalDensity;
+         break;
+      case DENSM :
+         selfnc=&GaussWaveFunction::EvalFTDensity;
+         break;
+      case MGRD :
+         selfnc=&GaussWaveFunction::EvalMagGradRho;
+         break;
+      case LAPD :
+         selfnc=&GaussWaveFunction::EvalLapRho;
+         break;
+      case ELFD :
+         selfnc=&GaussWaveFunction::EvalELF;
+         break;
+      case SENT :
+         selfnc=&GaussWaveFunction::EvalShannonEntropy;
+         break;
+      case SENTM :
+         selfnc=&GaussWaveFunction::EvalMomentumShannonEntropy;
+         break;
+      case KEDK :
+         selfnc=&GaussWaveFunction::EvalKineticEnergyK;
+         break;
+      case KEDKM :
+         selfnc=&GaussWaveFunction::EvalFTKineticEnergy;
+         break;
+      case KEDG :
+         selfnc=&GaussWaveFunction::EvalKineticEnergyG;
+         break;
+      case MGLD :
+         selfnc=&GaussWaveFunction::EvalMagGradLOL;
+         break;
+      case MEPD :
+         selfnc=&GaussWaveFunction::EvalMolElecPot;
+         break;
+      case MLED :
+         selfnc=&GaussWaveFunction::EvalMagLED;
+         break;
+      case REDG :
+         selfnc=&GaussWaveFunction::EvalReducedDensityGradient;
+         break;
+      case ROSE :
+         selfnc=&GaussWaveFunction::EvalRoSE;
+         break;
+      case VPED :
+         selfnc=&GaussWaveFunction::EvalVirialPotentialEnergyDensity;
+         break;
+      case NCIS :
+         selfnc=&GaussWaveFunction::EvalNCIs;
+         break;
+      case NCIL :
+         selfnc=&GaussWaveFunction::EvalNCILambda;
+         break;
+      case ELLPY :
+         selfnc=&GaussWaveFunction::EvalEllipticity;
+         break;
+      case DORI :
+         selfnc=&GaussWaveFunction::EvalDORI;
+         break;
+      case SCFD :
+         selfnc=&GaussWaveFunction::EvalCustomScalarField;
+         break;
+      default :
+         ScreenUtils::DisplayErrorMessage("Field not setup to be choosable!\n"
+               "Setting up electron deensity as the function.");
+         cout << __FILE__ << ", fnc: " << __FUNCTION__ << ", line: " << __LINE__ << '\n';
+         selfnc=&GaussWaveFunction::EvalDensity;
+         break;
+   }
+}
+void DTKScalarFunction::NumericalGradient(double x,double y,double z,double (&g)[3]) {
+   double hh=0.02;
+   double oo2h=0.5e0/hh;
+   double ffpp=f(x+hh,y,z);
+   ffpp-f(x-hh,y,z);
+   g[0]=oo2h*ffpp;
+   ffpp=f(x,y+hh,z);
+   ffpp-f(x,y-hh,z);
+   g[1]=oo2h*ffpp;
+   ffpp=f(x,y,z+hh);
+   ffpp-f(x,y,z-hh);
+   g[2]=oo2h*ffpp;
 }
--- src/common/dtkscalarfunction3d.h
+++ src/common/dtkscalarfunction3d.h
@@ -57,6 +57,9 @@ public:
          case ScalarFieldType::MEPD :
             return wf->EvalMolElecPot(x,y,z);
             break;
+         case ScalarFieldType::MLED :
+            return wf->EvalMagLED(x,y,z);
+            break;
          case ScalarFieldType::REDG :
             return wf->EvalReducedDensityGradient(x,y,z);
             break;
@@ -66,6 +69,12 @@ public:
          case ScalarFieldType::VPED :
             return wf->EvalVirialPotentialEnergyDensity(x,y,z);
             break;
+         case ScalarFieldType::NCIS :
+            return wf->EvalNCIs(x,y,z);
+            break;
+         case ScalarFieldType::NCIL :
+            return wf->EvalNCILambda(x,y,z);
+            break;
          case ScalarFieldType::EDFTA :
             trho=wf->EvalDensity(x,y,z);
             return (-0.75e0*pow((0.9549296585513720146e0*trho),1.0e0/3.0e0));
@@ -73,13 +82,16 @@ public:
          case ScalarFieldType::ELLPY :
             return wf->EvalEllipticity(x,y,z);
             break;
+         case ScalarFieldType::DORI :
+            return wf->EvalDORI(x,y,z);
+            break;
          case ScalarFieldType::SCFD :
             return wf->EvalCustomScalarField(x,y,z);
             break;
          default :
             if ( prntunknfld1st ) {
-               printf("Unknown field!\n");
-               printf("%s, line: %d\n",__FILE__,__LINE__);
+               printf("\033[31mUnknown field!\n");
+               printf("%s, line: %d\033[0m\n",__FILE__,__LINE__);
                prntunknfld1st=false;
             }
             return 0.0e0;
@@ -87,12 +99,49 @@ public:
       }
       return 0.0e0;
    }
+   inline void gradf(double x,double y,double z,double (&gs)[3]) {
+      double trho;
+      switch ( sft ) {
+         case ScalarFieldType::DENS :
+            wf->EvalRhoGradRho(x,y,z,trho,gs);
+            break;
+         case ScalarFieldType::LOLD :
+            double xx[3],h[3][3];
+            xx[0]=x; xx[1]=y; xx[2]=z;
+            wf->EvalHessLOL(xx,trho,gs,h);
+            break;
+         case ScalarFieldType::REDG :
+         case ScalarFieldType::NCIS :
+            wf->EvalGradReducedDensityGradient(x,y,z,gs);
+            break;
+         case ScalarFieldType::GLOL :
+         case ScalarFieldType::LEDV :
+         case ScalarFieldType::VCFD :
+            if ( prntunknfld1st ) {
+               printf("\033[31mThis gradient field cannot be computed!\n");
+               printf("In the current version, only the gradients of Rho, ReducedGradientDensity,\n"
+                     " and LOL are implemented...");
+               printf("%s, line: %d\033[0m\n",__FILE__,__LINE__);
+               prntunknfld1st=false;
+            }
+            gs[0]=gs[1]=gs[2]=0.0e0;
+            break;
+         default :
+            NumericalGradient(x,y,z,gs);
+            break;
+      }
+   }
+   double fptr(double x,double y,double z) {return (wf->*selfnc)(x,y,z);}
+   void NumericalGradient(double x,double y,double z,double (&g)[3]);
 /* ************************************************************************** */
 protected:
 /* ************************************************************************** */
+   void SelectScalarFunctionPtr(const char prop);
+   void SelectScalarFunctionPtr(const ScalarFieldType ft);
    ScalarFieldType sft;
    GaussWaveFunction* wf;
    static bool prntunknfld1st; /*!< Whether to print error messages.  */
+   double (GaussWaveFunction::*selfnc)(double,double,double);
 /* ************************************************************************** */
 };
 /* ************************************************************************** */
